107d287d57c8b458b775c94bb445e0868654bd68
Qamariddindev-001 <qamariddinol664@gmail.com>
2022-10-12 11:28:09 +0500

    bin/string_length01.dart

--- bin/string_length01.dart
+++ bin/string_length01.dart
@@ -5,7 +5,15 @@
         a: string
     Returns:
         length of string */
+int func(String s) {
+  int n;
+
+  n = s.length;
+
+  return n;
+}
 
 void main() {
+  print(func("Shahzod"));
   // write your code here
 }
