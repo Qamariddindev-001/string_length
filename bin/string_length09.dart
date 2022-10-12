c097f9af303b4b7d32a048b5a293b294f6854502
Qamariddindev-001 <qamariddinol664@gmail.com>
2022-10-12 16:40:26 +0500

    bin/string_length09.dart dastur tuzildi

--- bin/string_length09.dart
+++ bin/string_length09.dart
@@ -7,6 +7,14 @@
     Returns:
         str: answer
      */
+String func(String s1, String s2) {
+  if (s1.length >= s2.length) {
+    return s1 + s2;
+  }
+  return s1;
+}
+
 void main() {
   // write your code here
+  print(func('hello', 'coderr'));
 }
